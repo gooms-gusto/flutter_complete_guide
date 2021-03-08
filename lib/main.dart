56f18048c62224583bad3379e7eaad2147bfac4f
gooms-gusto <gooms.gusto@gmail.com>
2021-03-08 21:30:57 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Main page'),
+          ),
+          body: Column(
+            children: [
+              Text('The question'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: null,
+              ),
+            ],
+          )),
+    );
+  }
+}
