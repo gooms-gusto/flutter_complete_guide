2fee4ee525364774d161ec75ee6585fa47d8d98e
gooms-gusto <gooms.gusto@gmail.com>
2021-03-11 14:00:39 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,59 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var _questionindex = 0;
+
+  void _answer_question() {
+    setState(() {
+      _questionindex = _questionindex + 1;
+
+      if (_questionindex == 3) {
+        _questionindex = 0;
+      }
+    });
+    print(_questionindex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _listquestion = [
+      'Order classic cookies',
+      'Order dark chocolate',
+      'Order red velvet'
+    ];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('Main page'),
+            title: Text('Main Page'),
           ),
           body: Column(
             children: [
-              Text('The question'),
+              Question(_listquestion[_questionindex]),
               RaisedButton(
                 child: Text('Answer 1'),
-                onPressed: null,
+                onPressed: _answer_question,
               ),
               RaisedButton(
                 child: Text('Answer 2'),
-                onPressed: null,
+                onPressed: () => print('you have pressed button 2!'),
               ),
               RaisedButton(
                 child: Text('Answer 3'),
-                onPressed: null,
+                onPressed: () {
+                  print('you have pressed button 3!');
+                },
               ),
             ],
           )),
---
+++ lib/question.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Question extends StatelessWidget {
+  final String _questiontext;
+
+  Question(this._questiontext);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        _questiontext,
+        style: TextStyle(
+            color: Colors.black,
+            decoration: TextDecoration.underline,
+            decorationColor: Colors.red,
+            decorationStyle: TextDecorationStyle.wavy),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
